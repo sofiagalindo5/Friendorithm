5a9d3e4b73911ec211a904dc65dd655fe9521bde
andrewsolomon <andrewcsolomon26@gmail.com>
2026-03-16 22:28:55 -0500

    breadth first search

--- FriendAlgorithm/src/main.cpp
+++ FriendAlgorithm/src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "dataset.h"
 #include "graph.h"
-
+#include "bfs.h"
+#include <chrono>
 using namespace std;
 
 int main()
@@ -19,6 +20,14 @@ int main()
 
     cout << "Users: " << graph.getNumUsers() << endl;
     cout << "Edges: " << graph.getNumEdges() << endl;
+
+    auto start = chrono::high_resolution_clock::now();
+    int nodesVisited = 0;
+    vector<int> recs = bfsRecommend(graph, 25, 2, 10, nodesVisited);
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cout << "BFS runtime: " << duration.count() << " ms" << endl;
+    cout << "Nodes visited: " << nodesVisited << endl;
 
     return 0;
 }
